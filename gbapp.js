d5ac049d0b46a6d5034e2d1e764f6ec5ccbc0e60
VamsiNagendraPilli <S528374@mail.nwmissouri.edu>
2016-10-11 15:17:22 -0500

    Add files via upload

---
+++ gbapp.js
@@ -0,0 +1,24 @@
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+
+var app = express();  // make express app
+var Server = require('http').createServer(app);
+
+app.get('/',function(req,res) {
+	res.send('Hello World');
+});
+
+// set up the view engine
+app.set('view engine','ejs');
+// manage our entries
+// set up the logger
+// GETS
+// POSTS
+// 404
+
+// Listen for an application request on port 8081
+Server.listen(8081, function () {
+  console.log('Guestbook app listening on http://127.0.0.1:8081/');
+});
